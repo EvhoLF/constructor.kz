867e4086a9ca8697ebedf8c128a812bd16c1b104
EvhoLF <evholf@gmail.com>
2025-10-06 22:19:29 +0300

    dbLongText

---
+++ prisma/migrations/20251006191840_db_long_text/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE `templatediagram` MODIFY `nodes` LONGTEXT NULL,
+    MODIFY `edges` LONGTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `templateformula` MODIFY `nodes` LONGTEXT NULL,
+    MODIFY `edges` LONGTEXT NULL;
