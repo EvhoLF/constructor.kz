af7aa6d15cb447434f3af15878ed732991e1c04a
evholf <evholf@gmail.com>
2025-10-27 18:13:49 +0300

    migrateBD2

---
+++ prisma/migrations/20251027151144_fix_delete_image_from_kanban2/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `image` on the `Kanban` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Kanban` DROP COLUMN `image`;
